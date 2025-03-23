63c57b233583ae684d7c6b5741d52ce7c127a0a9
creative-pag <neha.ec.eng@gmail.com>
2025-03-23 13:42:01 -0700

    initial commit

---
+++ sortingtechniques/merge.py
@@ -0,0 +1,28 @@
+def mergesort(alist):
+    if len(alist) >1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+        mergesort(lefthalf)
+        mergesort(righthalf)
+        i= j= k=0
+        while i<len(lefthalf) and j<len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k]=lefthalf[i]
+                i+=1
+            else:
+                alist[k]= righthalf[j]
+                j+=1
+            k+=1
+        while i< len(lefthalf):
+            alist[k] = lefthalf[i]
+            i+=1
+            k+=1
+        while j< len(righthalf):
+            alist[k]= righthalf[j]
+            j+=1
+            k+=1
+        return alist
+marks = [1,8,5,13,75,16,20]
+result = mergesort(marks)
+print(result)
